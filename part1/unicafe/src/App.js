990ed9c7e96cd0ac286f49d4e31c05bf14606d22
iamjhp <jooho.park@outlook.com>
2022-05-01 02:13:27 +0200

    finish exercise unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value}
+      </td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const avg = total ? (good - bad) / total : 0
+  const positiv = total ? `${good * 100 / total} %` : `0 %`
+
+  if (total == 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine  text='good' value={good}/>
+        <StatisticLine  text='neutral' value={neutral}/>
+        <StatisticLine  text='bad' value={bad}/>
+        <StatisticLine  text='all' value={total}/>
+        <StatisticLine  text='average' value={avg}/>
+        <StatisticLine  text='positive' value={positiv}/>
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={increaseGood} text='good'/>
+      <Button onClick={increaseNeutral} text='neutral'/>
+      <Button onClick={increaseBad} text='bad'/>
+      <h1>statistics</h1>
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
+    </div>
+  )
+}
+
+export default App
