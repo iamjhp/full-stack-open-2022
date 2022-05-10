4934e74cb4acf43f2e2fb7abf86233803684915d
iamjhp <jooho.park@outlook.com>
2022-05-11 01:33:51 +0200

    finish part3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,22 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
+
 app.use(express.static('build'))
 app.use(cors())
-/*
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-*/
+
 app.use(express.json())
-//app.use(requestLogger)
-
-
 
 morgan.token('post', (request, response) => {
   return JSON.stringify(request.body)
@@ -25,56 +17,36 @@ morgan.token('post', (request, response) => {
 const morganTiny = morgan(':method :url :status :res[content-length] - :response-time ms :post')
 app.use(morganTiny)
 
-
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello Word</h1>')
 })
 
-app.get('/api/persons', (request, response, next) => {
-  response.json(persons)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id == id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.get('/info', (request, response, next) => {
-  const numberOfPersons = persons.length
-
-  response.send(
-    `<div>Phonebook has info for ${numberOfPersons} people</div>
-    ${new Date()}
-    `
-    )
+app.get('/info', (request, response) => {
+  Person.countDocuments({}).then(count => {
+    response.send(
+      `<div>Phonebook has info for ${count} people</div>
+      ${new Date()}
+      `
+      )
+  })
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -92,28 +64,44 @@ app.post('/api/persons', (request, response, next) => {
     })
   }
 
-  const newPerson = {
-    id: Math.floor(Math.random() * 9999999),
+  const newPerson = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  Person.exists({name: newPerson.name}).then(foundPersonId => {
+    if (foundPersonId) {
+      return response.status(400).send({
+        error: 'name must be unique'
+      })
+    }
+    newPerson.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+  })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
     name: body.name,
     number: body.number
   }
 
-  const foundName = persons.find(person => person.name === newPerson.name)
-  if (foundName) {
-    return response.status(400).send({
-      error: 'name must be uniqueSADADSADSADSADADSADSADSADASDSADSADSASDSADSADSAD'
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query'})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
     })
-  }
-
-  persons = persons.concat(newPerson)
-  response.json(newPerson)
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -122,9 +110,22 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message})
+  } 
 
-const PORT = process.env.PORT || 3001
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log("connecting to", url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log("connected to MongoDB")
+  }) 
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message)
+  })
+
+const personSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      minLength: 8,
+      required: true,
+      type: String,
+      validate: {
+        validator: function(v) {
+          const regex = new RegExp(/\d{3}-\d{3}-\d{4}/)
+          console.log(regex.test('123-123-12344'))
+          return /^\d{2,3}-\d+$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
+    }
+  },
+  {
+    collection: 'persons'
+  }
+)
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
