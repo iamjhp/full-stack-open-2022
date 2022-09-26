b2058fde504fee32821ef9d974cec9986cfc73a3
iamjhp <jooho.park@outlook.com>
2022-09-27 01:45:55 +0200

    finish 4.15

---
+++ part4/bloglist/tests/user_api.test.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const helper = require('./test_helper')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const User = require('../models/user')
+
+
+describe('when there is initially one user at DB', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('secret', 10)
+    const user = new User({ username: 'root', passwordHash})
+
+    await user.save()
+  })
+
+  test('a new user can be added', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'newuser',
+      name: 'test user',
+      password: 'pizza'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
