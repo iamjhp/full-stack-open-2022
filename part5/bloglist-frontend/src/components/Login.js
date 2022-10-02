c7e706703d49c7945cb10e755354dbd9de459331
iamjhp <jooho.park@outlook.com>
2022-10-02 11:49:26 +0200

    add part5 folder

---
+++ part5/bloglist-frontend/src/components/Login.js
@@ -0,0 +1,15 @@
+const Login = () => (
+  <div>
+    <h2>log in to application</h2>
+    <form>
+      <div>
+        username
+          <input 
+            type="text"
+          />
+      </div>
+    </form>
+  </div>
+)
+
+export default Login
